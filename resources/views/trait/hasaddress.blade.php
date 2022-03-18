f4049be6c8e2c8ada9b2d341658f509fbc503c2f
Kiersm <kieran@ascent-creative.co.uk>
2022-03-18 13:05:49 +0000

    renove wraoper

--- resources/views/trait/hasaddress.blade.php
+++ resources/views/trait/hasaddress.blade.php
@@ -7,7 +7,7 @@
   
 @endphp
 
-<div class="w-50">
+{{-- <div class="w-50"> --}}
     {{-- <h4>Address:</h4> --}}
     <x-cms-form-input type="text" label="Street 1" name="_address[street1]" value="{!! old('_address.street1', $addr->street1 ?? '') !!}"/>
     <x-cms-form-input type="text" label="Street 2" name="_address[street2]" value="{!! old('_address.street2', $addr->street2 ?? '') !!}"/>
@@ -19,5 +19,5 @@
     {{-- @dump($addr->country()->getRelated()); --}}
     <x-cms-form-relationautocomplete label="Country" :relationship="$addr->country()" displayField="name" name="_address[country_id]" dataurl="/countries/autocomplete" />
 
-</div>
+{{-- </div> --}}
 
