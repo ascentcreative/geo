e46834f201e5e77c5506f222bf2d848746b9fb38
Kiersm <kieran@ascent-creative.co.uk>
2022-03-18 13:02:43 +0000

    renove ttle for now

--- resources/views/trait/hasaddress.blade.php
+++ resources/views/trait/hasaddress.blade.php
@@ -8,7 +8,7 @@
 @endphp
 
 <div class="w-50">
-    <h4>Address:</h4>
+    {{-- <h4>Address:</h4> --}}
     <x-cms-form-input type="text" label="Street 1" name="_address[street1]" value="{!! old('_address.street1', $addr->street1 ?? '') !!}"/>
     <x-cms-form-input type="text" label="Street 2" name="_address[street2]" value="{!! old('_address.street2', $addr->street2 ?? '') !!}"/>
     <x-cms-form-input type="text" label="City" name="_address[city]" value="{!! old('_address.city', $addr->city ?? '') !!}"/>
