ee96a7edcf4d4639403872d1c5acea7a2fc91b61
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-07-21 17:57:37 +0100

    Integrate with Forms update

--- src/TraitForms/HasAddress.php
+++ src/TraitForms/HasAddress.php
@@ -28,14 +28,3 @@ class HasAddress extends Subform {
     }
 
 }
-
-
-// <x-cms-form-input type="text" label="Street 1" name="address[street1]" value="{!! old('address.street1', $addr->street1 ?? '') !!}"/>
-// <x-cms-form-input type="text" label="Street 2" name="address[street2]" value="{!! old('address.street2', $addr->street2 ?? '') !!}"/>
-// <x-cms-form-input type="text" label="City" name="address[city]" value="{!! old('address.city', $addr->city ?? '') !!}"/>
-// <x-cms-form-input type="text" label="County/State" name="address[state]" value="{!! old('address.state', $addr->state ?? '') !!}"/>
-// <x-cms-form-input type="text" label="Postcode/Zip" name="address[zip]" value="{!! old('address.zip', $addr->zip ?? '') !!}"/>
-// {{-- <x-cms-form-input type="text" label="Country" name="address[country_id]" value="{!! old('_address.country_id', $addr->country_id ?? '') !!}"/>     --}}
-
-// {{-- @dump($addr->country()->getRelated()); --}}
-// <x-cms-form-relationautocomplete label="Country" :relationship="$addr->country()" displayField="name" name="address[country_id]" dataurl="/countries/autocomplete" />
